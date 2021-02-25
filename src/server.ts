e4ac4ea6e6abafefd2cbe6f59d7762ec6b3822b5
priolilucas1 <priolilucas1@gmail.com>
2021-02-25 14:13:35 -0300

    Server connection changed

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
+import { router } from "./routes";
 import "reflect-metadata";
 import express from "express";
 import "./database";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Dados foram criados com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server is running! 🚀");
